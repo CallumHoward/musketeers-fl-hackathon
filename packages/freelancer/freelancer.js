49f895e92aa9428e61aeaa77bbffb57dd5453601
Albert Kurniawan <kurniawan.albert19@gmail.com>
2015-09-26 18:14:35 +1000

    Added MDL package and freelancer package.

---
+++ packages/freelancer/freelancer.js
@@ -0,0 +1,1 @@
+Freelancer = Npm.require('freelancer-node');
---
+++ packages/freelancer/package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'musketeers:freelancer',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  api.use('ecmascript');
+  api.addFiles('freelancer.js', 'server');
+  api.export('Freelancer', 'server');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('musketeers:freelancer');
+  api.addFiles('freelancer-tests.js');
+});
+
+Npm.depends({
+  "freelancer-node": "https://musketeersdummy:giveusmonies8@github.com/kAlbert19/freelancer-node/archive/66d3e313be922acc7351df9bbbb4c0a151b50ce9.tar.gz"
+});
