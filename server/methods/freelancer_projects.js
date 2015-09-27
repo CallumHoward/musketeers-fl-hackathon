5a1e3a35d5cfcd8cfd02ccc61a60e40f5f38ccf9
erickchaosz <erickwu8@hotmail.com>
2015-09-27 11:18:53 +1000

    added award bidder

--- server/methods/freelancer_projects.js
+++ server/methods/freelancer_projects.js
@@ -11,8 +11,30 @@ Meteor.methods({
     Freelancer.Projects.create(data, Meteor.bindEnvironment(function(err, res) {
       if (err) throw new Meteor.Error(400, err);
 
-      console.log(res);
-    }));
-  },
 
+      var project_id = res.result.id;
+      var budget = res.budget.minimum - 5; //budget min profit
+
+      Meteor.setTimeout(function() {
+        Freelancer.Projects.getBids(project_id, {}, Meteor.bindEnvironment(function(err, res) {
+          if (err) throw new Meteor.Error(400, err);
+
+          var bids = res.result.bids; // Array
+          var bestBid = _.filter(bids, function(item) {
+              return item.amount <= budget;
+          })[0]; //get best bidder
+
+          var bestBidderId = bestBid.id; //id of the best bidder
+
+          //award the bidder
+          Freelancer.Bids.performBid(bestBidderId, {action: "award"}, Meteor.bindEnvironment(function(err, res) {
+            if (err) throw new Meteor.Error(400, err);
+
+            
+          }));
+
+        }));
+      }, 1800000);
+    }));
+  }
 });
